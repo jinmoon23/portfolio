7ef4703062c2033fcce779f787c359e07daa7dc4
최진문 <wlsansdlek1@me.com>
2025-05-29 12:03:20 +0900

    Chore: gh-pages 브랜치 수정

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/portfolio/", // ← 여기에 실제 레포지토리명을 넣으세요!
-});
